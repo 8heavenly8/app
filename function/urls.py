98bda623d0b6d81ca884517b5db66e2d7563d920
Щемелёва Анастасия <ShcheAna_89@students.mgke.loc>
2026-06-20 15:46:58 +0300

    errors

--- function/urls.py
+++ function/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  
